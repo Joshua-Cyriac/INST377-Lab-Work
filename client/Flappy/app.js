a1460194f8a10052d3db06791b1f47b57a3d550b
Joshua-Cyriac <90971637+Joshua-Cyriac@users.noreply.github.com>
2021-09-21 19:41:02 -0400

    Update app.js

--- client/Flappy/app.js
+++ client/Flappy/app.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded' , () => {
     const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.gmae-container')
+    const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground')
 
     let birdLeft = 220
-    let birdBottom = 100
+    let birdBottom = 200
+    let gravity = 2
 
-    function startGame() {
-        bird.style.bottom = birdBottom  + 'px'
+
+    function StartGame() {
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
 
     }
-    startGame()
-})
+    let timerID = setInterval(StartGame, 20)
+}) 
