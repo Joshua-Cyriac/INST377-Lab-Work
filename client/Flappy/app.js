f5c1e9cb926780006cb6d5cad47fea9a65d2a0a4
Joshua-Cyriac <90971637+Joshua-Cyriac@users.noreply.github.com>
2021-09-22 13:40:12 -0400

    Update app.js

--- client/Flappy/app.js
+++ client/Flappy/app.js
@@ -16,12 +16,31 @@ document.addEventListener('DOMContentLoaded' , () => {
     }
     let timerID = setInterval(StartGame, 20)
 
-    function jump(){
-
-        bird.style.bottom = birdBottom + 'px'
-
-
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        } 
     }
 
+    function jump(){
+        if (birdBottom < 650) birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+
+    }
+    document.addEventListener('keyup', control)
+
+
+    function generateObtacle()  {
+        let obstacleLeft = 500
+        let obstacleBottom = 150 
+        const obstacle = document.createElement('div') 
+        obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft +  'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+    }
+
+    generateObtacle()
        
 }) 
