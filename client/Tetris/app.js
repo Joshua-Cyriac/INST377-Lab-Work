0364c85eac4c4992ed64a5a6dae3a2e822f27b87
Joshua-Cyriac <90971637+Joshua-Cyriac@users.noreply.github.com>
2021-10-04 11:27:15 -0400

    Create app.js

---
+++ client/Tetris/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContnentLoaded', () => {
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid dic'))
+const ScoreDisplay = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+const width = 10
+
+
+
+})
